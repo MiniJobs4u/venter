5fbcbc1f465e29c71ee91b5b73652c342abde305
JardaZ <jaroslavzid@yahoo.co.uk>
2025-08-05 15:47:08 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-import React, { useState, useEffect } from "react";
-import Register from "./Register";
-import Login from "./components/Login";
+import React from "react";
 import AdminPanel from "./components/AdminPanel";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const auth = getAuth();
-    const unsub = onAuthStateChanged(auth, (firebaseUser) => {
-      setUser(firebaseUser);
-    });
-    return () => unsub();
-  }, []);
-
-  if (window.location.pathname === "/admin") {
-    return user ? <AdminPanel /> : <Login onLogin={() => window.location.reload()} />;
-  }
-
-  return <Register />;
+  return <AdminPanel />;
 }
 
 export default App;
+
