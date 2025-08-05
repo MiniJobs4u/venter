39cf284defe81e88b214c091254d72b3189ac1c7
JardaZ <jaroslavzid@yahoo.co.uk>
2025-08-05 21:13:19 +0100

    Update AdminPanel.jsx

--- src/AdminPanel.jsx
+++ src/AdminPanel.jsx
@@ -1,1 +1,13 @@
+import React from "react";
+
+export default function AdminPanel() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h2>Admin Panel</h2>
+      <p>Welcome, admin! You are now logged in.</p>
+
+      {/* Můžeš sem přidat cokoliv dalšího – tabulky, tlačítka, data, atd. */}
+    </div>
+  );
+}
 
