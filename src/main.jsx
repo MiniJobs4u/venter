3af8e51ed91265607d32ab5ecc05ba4b01de015a
JardaZ <jaroslavzid@yahoo.co.uk>
2025-08-12 14:18:28 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-// src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import App from "./App.jsx";
+import RegistrationForm from "./components/RegistrationForm.jsx";
+import AdminLogin from "./components/AdminLogin.jsx";
+import AdminPanel from "./components/AdminPanel.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<RegistrationForm />} />
+          <Route path="/admin-login" element={<AdminLogin />} />
+          <Route path="/admin" element={<AdminPanel />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
